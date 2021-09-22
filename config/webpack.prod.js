3874b5b620c6191774cf10938e436fec2319fdc8
shenshuping <184341327@qq.com>
2021-09-22 16:39:49 +0800

    解决babel react ts jsx等相关的方式的支持问题

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const { DefinePlugin } = require("webpack");
 const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   mode: "production",
   plugins: [
+    new CleanWebpackPlugin(),
     new DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production"),
     }),
---
+++ src/IndexPage/views/Main.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useHistory,
+  useLocation,
+  useParams,
+} from "react-router-dom";
+
+interface Props {}
+function Home() {
+  return <h2>123123</h2>;
+}
+
+function About() {
+  return <h2>About1</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+export default function Main({}: Props): React.ReactElement {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/IndexPage">Home</Link>
+            </li>
+            <li>
+              <Link to="/IndexPage/about">About</Link>
+            </li>
+            <li>
+              <Link to="/IndexPage/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/IndexPage/about" exact>
+            <About />
+          </Route>
+          <Route path="/IndexPage/users">
+            <Users />
+          </Route>
+          <Route path="/IndexPage">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
