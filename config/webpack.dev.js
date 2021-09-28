ea8229855a8b475c503c067362132ebf124a0263
dagedan <dagedan@gmail.com>
2021-09-28 17:32:23 +0800

    解决发布引用js css的路径问题

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { DefinePlugin } = require("webpack");
 const Dotenv = require("dotenv-webpack");
 const getEntry = require("./utils");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const rewritesRules = () => {
   let rewrites = [];
@@ -14,12 +15,37 @@ const rewritesRules = () => {
     { from: /./, to: "/NotFound/index.html" },
   ];
 };
+
+
+const addEntry = () => {
+  let entryObj = {};
+  getEntry().forEach((item) => {
+    entryObj[item] = {
+      import: path.join(path.dirname(__dirname), "src", item, "index.jsx"),
+      filename: path.join(item, item) + ".js",
+    };
+  });
+  return entryObj;
+};
+
+const pageExtractCssArray = [];
+getEntry().forEach((item) => {
+  pageExtractCssArray.push(
+    new MiniCssExtractPlugin({ filename: item + "/[name].[contenthash].css" })
+  );
+});
+
 module.exports = {
   mode: "development",
+  entry: addEntry(),
   devServer: {
+    hot: true,
     open: true,
     historyApiFallback: {
       rewrites: rewritesRules(),
+    },
+    client: {
+      progress: true,
     },
     port: 9000,
   },
@@ -30,6 +56,31 @@ module.exports = {
     new Dotenv({
       path: path.resolve(__dirname, "..", "./.env.development"),
     }),
+    new MiniCssExtractPlugin({
+      filename: (item) => {
+        return path.join(item.chunk.name, item.chunk.name) + ".css";
+      },
+    }),
   ],
   devtool: "eval-source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          name: "common",
+          filename: (e) => {
+            return path.join("vendors", "common") + ".js";
+          },
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 };
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,11 +2,31 @@ const path = require("path");
 const { DefinePlugin } = require("webpack");
 const Dotenv = require("dotenv-webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const getEntry = require("./utils");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const addEntry = () => {
+  let entryObj = {};
+  getEntry().forEach((item) => {
+    entryObj[item] = {
+      import: path.join(path.dirname(__dirname), "src", item, "index.jsx"),
+      filename: path.join("../dist", item, item) + ".js",
+    };
+  });
+  return entryObj;
+};
+
+const pageExtractCssArray = [];
+getEntry().forEach((item) => {
+  pageExtractCssArray.push(
+    new MiniCssExtractPlugin({ filename: item + "/[name].[contenthash].css" })
+  );
+});
 module.exports = {
   mode: "production",
   devtool: "source-map",
   mode: "production",
+  entry: addEntry(),
   plugins: [
     new CleanWebpackPlugin(),
     new DefinePlugin({
@@ -15,9 +35,34 @@ module.exports = {
     new Dotenv({
       path: path.resolve(__dirname, "..", "./.env.production"),
     }),
+    new MiniCssExtractPlugin({
+      filename: (item) => {
+        return path.join("../dist", item.chunk.name, item.chunk.name) + ".css";
+      },
+    }),
   ],
   // output: {
   //   path: path.resolve(__dirname, '..', './dist'),
   //   filename: 'bundle.js'
-  // }
+  // },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          name: "common",
+          filename: (e) => {
+            return path.join("../dist", "vendors", "common") + ".js";
+          },
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 };
